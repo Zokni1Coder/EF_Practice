8e655e0ef76f0561d136cd18e34a6d5a9a6fa6fe
Zokni1Coder <erikkovacs0000@gmail.com>
2025-10-01 13:15:22 +0200

    Data Annotations for Motherboard class

--- EF_Practice/Models/Motherboard.cs
+++ EF_Practice/Models/Motherboard.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,11 +9,17 @@ namespace EF_Practice.Models
 {
     public class Motherboard
     {
+        [Column(name:"MotherboardID",Order = 0)]
         public int id { get; set; }
+        [Column(Order = 1)]
         public Manufacturer manufacturer { get; set; }
+        [Column(Order = 2)]
         public string model { get; set; }
+        [Column(Order = 3)]
         public CPU_Socket socket { get; set; }
+        [Column(Order = 5)]
         public Motherboard_form_factor motherboard_form_factor { get; set; }
+        [Column(Order = 4)]
         public Motherboard_chipset motherboard_chipset { get; set; }
         public int memory_max { get; set; }
         public RAM_type RAM_Type { get; set; }
@@ -22,6 +29,7 @@ namespace EF_Practice.Models
         public bool wifi { get; set; }
         public bool raid_supp { get; set; }
         public int price { get; set; }
+        [Column(Order = 6)]
         public ICollection<M2> M2s { get; set; }
     }
 }
