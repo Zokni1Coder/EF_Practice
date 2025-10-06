a0ce235d2b94736f6dcbbcd08c67f3e88639d4d9
Zokni1Coder <erikkovacs0000@gmail.com>
2025-10-06 21:10:46 +0200

    Database migration

--- EF_Practice/Models/Manufacturer.cs
+++ EF_Practice/Models/Manufacturer.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,7 +13,7 @@ namespace EF_Practice.Models
         public int id { get; set; }
         [Required, MaxLength(25)]
         public string name { get; set; }
-        public ICollection<CPU> CPUs { get; set; }
+        public ICollection<CPU> CPUs { get; set; }  
         public ICollection<CPU_Cooler> CPU_Coolers { get; set; }
         public ICollection<GPU> GPUs { get; set; }
         public ICollection<Motherboard> motherboards { get; set; }
